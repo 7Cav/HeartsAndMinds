2f20c892693b90e9ccbdd5b93956e353a0151ed6
NikPoliakov <niki.liber@gmail.com>
2021-11-29 17:26:38 +0200

    build script for Linux

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -d "./PBO" ]
+then
+    echo "PBO directory exists"
+    rm -rfv ./PBO/*
+else
+    mkdir "PBO"
+fi
+
+if [ -d "./WorkDir" ]
+then
+    echo "WorkDir directory exists"
+    rm -rfv ./WorkDir/*
+else
+    mkdir "WorkDir"
+fi
+
+for map in ./Missionbasefiles/*; do
+    echo "$map"
+    cp -rv $map ./WorkDir/
+done
+
+for map in ./WorkDir/*; do
+    echo "$map"
+    map_name=$(basename $map)
+    echo $map_name
+    cp -rv ./Missionframework/* $map/
+    mv -v $map/param.hpp $map/core/def/
+    JAPM $map ./PBO/$map_name
+done
